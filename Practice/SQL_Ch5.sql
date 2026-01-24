c23e35281160b73ab4f3e0954c44715ad21b64e9
Nikhil P Marihal <nikhilpmarihal9@gmail.com>
2026-01-24 08:13:09 +0530

    Add files via upload

---
+++ Practice/SQL_Ch5.sql
@@ -0,0 +1,16 @@
+-- DML COMMANDS
+-- INSERT COMMAND
+
+--  UPDATE COMMAND
+
+UPDATE customers
+SET name = 'Nikhil'
+WHERE id = 101;
+
+SELECT * FROM customers
+
+-- DELETE COMMAND
+DELETE FROM customers 
+WHERE email = 'aa';
+
+SELECT * FROM customers
